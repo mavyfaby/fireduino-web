e2eb50a2c5c7e491b4385e318967836835e8015a
mavyfaby <maverickfabroa@gmail.com>
2023-04-23 12:24:01 +0800

    refactor(anim): improve animation

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -2,19 +2,16 @@
   <div>
     <div class="main-container">
       <div class="flex flex-col">
-        <div ref="tx1" class="text-sm lg:text-base" />
-        <h2 ref="tx2" class="text-5xl lg:text-6xl font-bold my-7" />
+        <div ref="tx1" class="h-6 text-sm lg:text-base" />
+        <h2 ref="tx2" class="h-12 text-5xl lg:text-6xl font-bold my-7" />
         <h6 ref="tx3" class="text-sm mx-10 lg:mx-0 lg:text-lg tracking-wide leading-6" />
         <div class="flex justify-center lg:justify-start">
           <md-filled-button ref="el1" class="invisible opacity-0 mt-8" label="Get Started" />
         </div>
       </div>
-      <div class="flex justify-center lg:justify-end">
-        <img
-          ref="el2"
-          :src="store.dark ? previewImageDark : previewImageLight"
-          class="invisible opacity-0 w-64 lg:w-[300px] mt-16"
-        />
+      <div ref="el2" class="invisible flex justify-center lg:justify-end">
+        <img :src="previewImageDark" class="w-64 lg:w-[300px] mt-16 absolute" :class="store.dark ? 'opacity-1' : 'opacity-0'" />
+        <img :src="previewImageLight" class="w-64 lg:w-[300px] mt-16" :class="store.dark ? 'opacity-0' : 'opacity-1'" />
       </div>
     </div>
   </div>
@@ -34,7 +31,7 @@ const store = useStore();
 const strings = [
   "INTRODUCING",
   "Fireduino",
-  "An advanced fire detection and extinguishing systems, along with automated alerts to the nearest fire station",
+  "An advanced fire detection and extinguishing systems, along with automated alerts to the nearest fire station!",
 ];
 
 const tx1 = ref<HTMLElement | null>(null);
@@ -48,89 +45,97 @@ onMounted(() => {
   const tx2Text = strings[1];
   const tx3Text = strings[2];
 
-  anime({
+  // Animation timeline
+  const tl = anime.timeline();
+
+  tl.add({
     targets: tx1.value,
     translateY: [-50, 0],
     scale: [0.75, 1],
     delay: 1000,
-    duration: 2000,
-    easing: 'easeOutElastic(0.5, 0.4)'
-  })
-
-  anime({
-    targets: tx2.value,
-    translateY: [-50, 0],
-    scale: [0.75, 1],
-    delay: 2100,
-    duration: 2000,
-    easing: 'easeOutElastic(0.5, 0.4)'
-  });
-
-  anime({
-    targets: tx3.value,
-    translateY: [-50, 0],
-    scale: [0.75, 1],
-    delay: 3500,
-    duration: 2000,
-    easing: 'easeOutElastic(0.5, 0.4)'
-  });
-
-  new Typed(tx1.value, {
-    strings: [tx1Text],
-    startDelay: 1000,
-    typeSpeed: 50,
-    showCursor: false,
-  });
-
-  new Typed(tx2.value, {
-    strings: [tx2Text],
-    startDelay: 2100,
-    typeSpeed: 50,
-    showCursor: false,
-  });
-
-  new Typed(tx3.value, {
-    strings: [tx3Text],
-    typeSpeed: 15,
-    startDelay: 3500,
-    showCursor: false,
-    onComplete() {
-      el1.value?.classList.remove("invisible");
-
-      anime({
-        targets: el1.value,
-        translateY: [-20, 0],
-        scale: [0.5, 1],
-        opacity: [0, 1],
-        duration: 1000,
-        easing: 'easeOutElastic(0.5, 0.4)',
-        complete() {
-          
-          el2.value?.classList.remove("invisible");
-
-          anime({
-            targets: el2.value,
-            translateY: [-20, 0],
-            scale: [0.5, 1],
-            opacity: [0, 1],
-            duration: 2000,
-            easing: 'easeOutElastic(1, 0.8)',
-            complete() {
-              
-              anime({
-                targets: el2.value,
-                translateY: 20,
-                duration: 2000,
-                loop: true,
-                direction: 'alternate',
-                easing: 'linear',
-              });
-            }
-          });
-        }
+    duration: 1250,
+    easing: 'easeOutElastic(0.5, 0.4)',
+    begin() {
+      new Typed(tx1.value, {
+        strings: [tx1Text],
+        startDelay: 900,
+        typeSpeed: 50,
+        showCursor: false,
       });
     }
   });
+
+  tl.add({
+    targets: tx2.value,
+    translateY: [-50, 0],
+    scale: [0.75, 1],
+    rotateZ: [90, 0],
+    duration: 1200,
+    easing: 'easeOutElastic(0.5, 0.4)',
+    begin() {
+      new Typed(tx2.value, {
+        strings: [tx2Text],
+        typeSpeed: 50,
+        showCursor: false,
+      });
+    }
+  });
+
+  tl.add({
+    targets: tx3.value,
+    translateY: [-50, 0],
+    scale: [0.75, 1],
+    duration: 1000,
+    easing: 'easeOutElastic(0.5, 0.4)',
+    begin() {
+      new Typed(tx3.value, {
+        strings: [tx3Text],
+        typeSpeed: 15,
+        showCursor: false,
+        onComplete() {
+          el1.value?.classList.remove("invisible");
+        }
+      });
+    },
+  });
+
+  tl.add({
+    targets: el1.value,
+    translateY: [-20, 0],
+    scale: [0.5, 1],
+    rotateZ: [90, 0],
+    opacity: [0, 1],
+    delay: 2000,
+    duration: 1000,
+    easing: 'easeOutElastic(0.5, 0.4)',
+    complete() {
+      el2.value?.classList.remove("invisible");
+    }
+  });
+
+
+  tl.add({
+    targets: el2.value,
+    translateY: [-20, 0],
+    scale: [0.5, 1],
+    opacity: [0, 1],
+    duration: 2000,
+    easing: 'easeOutElastic(1, 0.8)',
+    complete() {
+      
+      // Loop animation
+      anime({
+        targets: el2.value,
+        translateY: 30,
+        duration: 2000,
+        loop: true,
+        direction: 'alternate',
+        easing: 'linear',
+      });
+
+    }
+  });
+
 });
 </script>
 
@@ -138,5 +143,9 @@ onMounted(() => {
 .main-container {
   @apply flex flex-col text-center lg:grid grid-cols-2
     lg:gap-10 lg:text-start lg:flex-row items-center mt-16 p-4;
+}
+
+img {
+  @apply shadow-xl shadow-neutral-300/50 dark:shadow-neutral-700/50 rounded-2xl;
 }
 </style>
